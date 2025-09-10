d60c9975774015f255bf3c26524a14816998f6b0
nichovesakha81 <studynicho@gmail.com>
2025-09-10 07:58:30 +0700

    Update views.py and main.html to use dynamic template variables, not hardcoded

--- main/views.py
+++ main/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'npm' : '240123456',
-        'name': 'Haru Urara',
+        'npm' : '2406495804',
+        'name': 'Nicholas Vesakha',
         'class': 'PBP A'
     }
 
